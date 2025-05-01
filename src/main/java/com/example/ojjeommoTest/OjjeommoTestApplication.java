336251ab04d5a6785b3279402941868868da32c3
KimYangSeon <endol_@naver.com>
2025-05-01 14:59:11 +0900

    project setting

---
+++ src/main/java/com/example/ojjeommoTest/OjjeommoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ojjeommoTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjjeommoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjjeommoTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ojjeommoTest/OjjeommoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ojjeommoTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjjeommoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
