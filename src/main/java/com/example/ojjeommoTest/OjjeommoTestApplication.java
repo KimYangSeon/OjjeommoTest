1e61a275e92e0754bbe61136be0dddfa2051a7f1
KimYangSeon <endol_@naver.com>
2025-05-13 20:08:10 +0900

    Action Test

--- src/main/java/com/example/ojjeommoTest/OjjeommoTestApplication.java
+++ src/main/java/com/example/ojjeommoTest/OjjeommoTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OjjeommoTestApplication {
-
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(OjjeommoTestApplication.class, args);
 	}
